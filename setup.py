bed08d13e21880af2cb4f210401c8fc034ddf7d5
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2020-11-05 08:51:56 -0600

    updated version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=["clipkit"],
     classifiers=CLASSIFIERS,
     entry_points={"console_scripts": ["clipkit = clipkit.clipkit:main"]},
-    version="0.1.6",
+    version="0.1.7",
     include_package_data=True,
     install_requires=REQUIRES,
 )
